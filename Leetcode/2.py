87387fbc62a7465d8bd97257f57deafca1747a61
Baisampayan1324 <baisampayandey1999@gmail.com>
2025-05-11 00:59:21 +0530

    Updateb2.py

--- Leetcode/2.py
+++ Leetcode/2.py
@@ -1,3 +1,30 @@
+# 🧾 Code:
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy_head = ListNode()
+        current = dummy_head
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry, digit = divmod(total, 10)
+
+            current.next = ListNode(digit)
+            current = current.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+
+        return dummy_head.next
+
 """
 Problem: Add Two Numbers
 
@@ -44,32 +71,4 @@ We just walk through both lists at the same time, add their values (plus carry),
   We create a new list to store the result, which could be as long as the longer input.
 
 ---
-
-🧾 Code:
 """
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution:
-    def addTwoNumbers(self, l1, l2):
-        dummy_head = ListNode()
-        current = dummy_head
-        carry = 0
-
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-
-            total = val1 + val2 + carry
-            carry, digit = divmod(total, 10)
-
-            current.next = ListNode(digit)
-            current = current.next
-
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
-
-        return dummy_head.next
